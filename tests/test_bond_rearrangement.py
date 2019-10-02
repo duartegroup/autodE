312f7438e9d0b338249bb0a0a3d9ef472df3ba0e
Joseph Silcock <joseph.silcock@st-hughs.ox.ac.uk>
2019-10-02 09:13:58 +0100

    Added tests

--- tests/test_bond_rearrangement.py
+++ tests/test_bond_rearrangement.py
@@ -1,11 +1,15 @@
 from autode import bond_rearrangement
 
+# Reaction H + H2 -> H2 + H
+rearrag = bond_rearrangement.BondRearrangement(
+    forming_bonds=[(0, 1)], breaking_bonds=[(1, 2)])
+
 
 def test_bondrearr_obj():
 
-    # Reaction H + H2 -> H2 + H
-    rearrag = bond_rearrangement.BondRearrangement(forming_bonds=[(0, 1)], breaking_bonds=[(1, 2)])
     assert rearrag.n_fbonds == 1
     assert rearrag.n_bbonds == 1
 
-    # TODO finish this
+    rearrag2 = bond_rearrangement.BondRearrangement(
+        forming_bonds=[(0, 1)], breaking_bonds=[(1, 2)])
+    assert rearrag2 == rearrag
