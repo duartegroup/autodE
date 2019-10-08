96a68721047ca888453519cc4d843da7dd96f58e
Unknown <ty13946@my.bristol.ac.uk>
2019-10-08 11:59:41 +0100

    Set ORCA available

--- tests/test_1d_pes_scan.py
+++ tests/test_1d_pes_scan.py
@@ -15,6 +15,9 @@ def test_get_ts_guess_1dscan():
     mol = Molecule(name='h3', xyzs=h3_xyzs, mult=2)
     mol.method = ORCA
 
+    # May not actually be available, but the .out files are included so this should work
+    mol.method.available = True
+
     ts_guess = get_ts_guess_1dpes_scan(mol=mol, active_bond=(1, 2), n_steps=5, name='h3_subst',
                                        reaction_class=Substitution, method=ORCA, keywords=Config.ORCA.scan_keywords,
                                        delta_dist=-0.3, active_bonds_not_scanned=[(0, 1)])
