5b727f469ef68ea029d494dccb3d20ddc8ad1e10
Joseph Silcock <joseph.silcock@st-hughs.ox.ac.uk>
2020-06-23 14:35:01 +0100

    Comments

--- autode/smiles/smiles_parser.py
+++ autode/smiles/smiles_parser.py
@@ -27,7 +27,7 @@ bond_order_symbol_dict = {'-': 1, '=': 2, '#': 3, '$': 4}
 class SmilesParser:
 
     def divide_smiles(self, string_to_divide):
-        """Divides a SMILES string into its constituent parts.
+        """Divides a SMILES string into its constituent sections.
            These are:
                      Atom (e.g 'Cl')
                      Bond, showing the bond order of a bond (e.g '=')
@@ -39,66 +39,91 @@ class SmilesParser:
         Args:
             string_to_divide (str): string to divide
         Yields:
-            (str, str): part of smiles string, what type of part it is
+            (str, str): section of smiles string, what type of section it is
         """
         smiles_chars = iter(string_to_divide)
         next_char = None
         third_char = None
+        # run through each character in the SMILES string, detect what section it
+        # belongs to (Atom, Bond etc.), then return the entire section and its type.
         while True:
             char_type = 'other'
+            # take a look at the next two characters to help identify the section of a
+            # SMILES string it is. We add the next character onto the first if they
+            # are part of the same section, so sometimes the next char will be None
             char = next(smiles_chars, '') if next_char is None else next_char
             next_char = next(smiles_chars, '') if third_char is None else third_char
             third_char = None
+
             if char == '':
+                # reached the end of the string
                 break
+
             if char in atoms_and_electrons.keys():
                 char_type = 'atom'
                 if (char + next_char) in atoms_and_electrons.keys():
+                    # e.g Cl, atom is Cl not C
                     char += next_char
                     next_char = next(smiles_chars, '')
+
             elif char in ['-', '=', '#', '$']:
                 char_type = 'bond'
+
             elif char == '[':
                 char_type, open_bracket, close_bracket = 'bracket_atom', '[', ']'
+
             elif char == '(':
                 char_type, open_bracket, close_bracket = 'branch', '(', ')'
+
             elif char in ['/', '\\']:
                 char_type = 'double_bond_stereochem'
-            # get the whole contents of the bracket
+
             if char_type in ['bracket_atom', 'branch']:
+                # want to get the whole contents of the a bracket, so add charcters until we
+                # have the same number of opening brackets as closing brackets
                 brackets = 1
                 while not brackets == 0:
                     if next_char == open_bracket:
                         brackets += 1
+
                     if next_char == close_bracket:
                         brackets -= 1
                     char += next_char
                     next_char = next(smiles_chars, '')
-            # get ring/multiple bond information about the atom, which comes after the atom
+
             if char_type in ['atom', 'bracket_atom', 'branch']:
+                # get ring/multiple bond information about the atom, which comes after the atom
                 while next_char.isdigit() or next_char == '%':
+                    # ring information is shown by assigning a number. If the number has
+                    # two digits, % preceeds the number
                     char += next_char
                     next_char = next(smiles_chars, '')
+
                 if next_char in ['-', '=', '#', '$']:
+                    # bond information can come in the middle of the ring information
                     third_char = next(smiles_chars, '')
                     if third_char.isdigit() or third_char == '%':
+                        # need to check for ring again
                         char += next_char
                         next_char = third_char
                         third_char = None
                         while next_char.isdigit() or next_char == '%':
                             char += next_char
                             next_char = next(smiles_chars, '')
+
             yield char, char_type
 
     def analyse_char(self, char, char_type):
-        """Analyse a part of a smiles string, depending on what part it is
+        """Analyse a section of a smiles string, depending on what section it is
         Args:
-            char (str): the part of a string to analyse
+            char (str): the section of a string to analyse
             char_type (str): the type of string
         """
         if char_type == 'atom':
+            # add_atom returns the bond information at the end of the atom string
             bond_ring_string = self.add_atom(char)
             self.analyse_bond_ring_string(bond_ring_string)
+
         elif char_type == 'bracket_atom':
             bracket_string, bond_ring_string = char.split(']')
             # remove any atom classes, we don't care about them. Also remove preceding '['
@@ -106,22 +131,28 @@ class SmilesParser:
             atom_details_string = self.add_atom(bracket_string)
             self.analyse_atom_details(atom_details_string)
             self.analyse_bond_ring_string(bond_ring_string)
+
         elif char_type == 'branch':
             if char[-1] != ')':
+                # must have ring information at the end
                 bond_ring_string = char.split(')')[-1]
                 branch_smiles = ''.join(char[1:].split(')')[:-1])
                 self.analyse_bond_ring_string(bond_ring_string)
+
             else:
                 branch_smiles = char[1: -1]
             before_branch_prev_atom_no = self.prev_atom_no
+
             # effectively another smiles string, just analyse it was we did the original
             for branch_smiles_char, branch_smiles_char_type in self.divide_smiles(branch_smiles):
                 self.analyse_char(branch_smiles_char, branch_smiles_char_type)
             self.prev_atom_no = before_branch_prev_atom_no
+
         elif char_type == 'bond':
             # the next bond we add will have this bond order
             next_bond_index = len(self.bonds)
             self.bond_order_dict[next_bond_index] = bond_order_symbol_dict[char]
+
         elif char_type == 'double_bond_stereochem':
             # could relate to the previous or next atom, sort out which later, as we can't know until the bonds and atoms have all been analysed
             next_atom_index = len(self.atoms)
@@ -136,12 +167,15 @@ class SmilesParser:
         """
         if self.atom_no != 0:
             self.bonds.append((self.prev_atom_no, self.atom_no))
+
         if atom_string[:2] in atoms_and_electrons.keys():
             label = atom_string[:2]
             rest_of_string = atom_string[2:]
+
         else:
             label = atom_string[0]
             rest_of_string = atom_string[1:]
+
         self.atoms.append(Atom(label, 0.0, 0.0, 0.0))
         self.prev_atom_no = self.atom_no
         self.atom_no += 1
@@ -163,24 +197,31 @@ class SmilesParser:
         while str_exists:
             ring_char = next(it, '')
             if ring_char == '':
+                # end of string
                 str_exists = False
                 break
+
             if ring_char == '%':
+                # next two characters are the ring number
                 ring_no = int(next(it) + next(it))
+
             else:
                 ring_no = int(ring_char)
+
             # first time we encounter the ring number, put it in the dictionary with the atom it refers to
-            # second time we encounter the ring number, form the bond
+            # second time we encounter the ring number, form the bond between the two atoms it references
             if ring_no in self.ring_dict.keys():
                 ring_atom, mult_bond = self.ring_dict.pop(ring_no)
                 bond_index = len(self.bonds)
                 self.bonds.append((ring_atom, self.prev_atom_no))
+                # ring closing bonds have a different priority in stereochem
                 self.ring_closing_bond_list.append(bond_index)
                 # bond order can be mentioned in either/both of the ring specifications
                 if mult_bond is not None:
                     self.bond_order_dict[bond_index] = mult_bond
                 elif multiple_bond is not None:
                     self.bond_order_dict[bond_index] = multiple_bond
+
             else:
                 self.ring_dict[ring_no] = (self.prev_atom_no, multiple_bond)
 
@@ -199,28 +240,38 @@ class SmilesParser:
             detail_char = next(it, '') if next_detail_char is None else next_detail_char
             next_detail_char = next(it, '')
             if detail_char == '':
+                # end of string
                 str_exists = False
                 break
+
             elif detail_char == 'H':
                 if next_detail_char.isdigit():
+                    # next digit can say how many Hs there are
                     hydrogens = int(next_detail_char)
                     next_detail_char = None
                 else:
                     hydrogens = 1
-            # charge only does up to +-9, shouldn't be an issue?
+
+            # charge only does up to +-9, shouldn't be an issue? tehcnically need to go to +-15
+            # but this seems unlikely to be used
             elif detail_char == '+':
                 if next_detail_char.isdigit():
+                    # next digit can say the number of the charge
                     charge += int(next_detail_char)
                     next_detail_char = None
                 else:
                     charge += 1
+
             elif detail_char == '-':
                 if next_detail_char.isdigit():
+                    # next digit can say the number of the charge
                     charge -= int(next_detail_char)
                     next_detail_char = None
                 else:
                     charge -= 1
+
             elif detail_char == '@':
+                # stereochem either @ or @@
                 if next_detail_char == '@':
                     stereochem = detail_char + next_detail_char
                     next_detail_char = None
@@ -241,14 +292,19 @@ class SmilesParser:
         h_bonds = []
         for i, atom in enumerate(self.atoms):
             n_bonds = 0
+            # count the number of bonds this atom has
             for j, bond in enumerate(self.bonds):
                 if i in bond:
                     if j in self.bond_order_dict.keys():
+                        # include double bonds as two bonds etc
                         n_bonds += self.bond_order_dict[j]
                     else:
                         n_bonds += 1
+
             if i in self.hydrogen_dict.keys():
+                # number of hydrogens explicitly stated
                 n_hydrogen = self.hydrogen_dict[i]
+
             else:
                 if atom.label in hs.keys():
                     # these atoms automatically fill their valence with H unless they come from a bracket atom ([C]), in which case the number of H is specified
@@ -259,12 +315,14 @@ class SmilesParser:
                             break
                 else:
                     n_hydrogen = 0
+
             for _ in range(n_hydrogen):
                 h_atoms.append(Atom('H', 0.0, 0.0, 0.0))
                 h_bonds.append((i, self.atom_no))
                 n_bonds += 1
                 self.atom_no += 1
             n_electrons = atoms_and_electrons[atom.label] - n_bonds - self.charge_dict.get(i, 0)
+
             if n_electrons % 2 == 1:
                 self.n_radical_electrons += 1
         self.atoms += h_atoms
@@ -280,6 +338,7 @@ class SmilesParser:
             bond = self.bonds[bond_no]
             for i, atom in enumerate(bond):
                 bonded_atoms = [bond[1-i]]
+                # find all bonded atoms to this atom
                 for other_bond in self.bonds:
                     if bond == other_bond:
                         continue
@@ -288,41 +347,59 @@ class SmilesParser:
                             bonded_atoms.append(other_bond[1])
                         if other_bond[1] == atom:
                             bonded_atoms.append(other_bond[0])
+
                 if len(bonded_atoms) != 3:
                     continue
+
                 if atom in self.alkene_stereochem_dict.keys():
-                    # as we assigned the stereochem to the next atom earlier, we now need to move it back to the right atoms
+                    # as we assigned the stereochem to the atom after it in the string earlier,
+                    # we now need to move it back to the right atoms
                     stereochem_symbol = self.alkene_stereochem_dict.pop(atom)
                     if bonded_atoms[-1] < atom:
+                        # this means both bonded atoms preceed the alkene atom in the SMILES string,
+                        # so stereochem information currently assigned to the alkene atom refers to
+                        # the atom before, and any stereochem information assigned to the atom before
+                        # refers to the atom before that
                         if bonded_atoms[-1] in self.alkene_stereochem_dict.keys():
                             other_stereochem_symbol = self.alkene_stereochem_dict.pop(bonded_atoms[-1])
                             self.alkene_stereochem_dict[bonded_atoms[-2]] = other_stereochem_symbol
                         self.alkene_stereochem_dict[bonded_atoms[-1]] = stereochem_symbol
+
                     else:
+                        # only one atom preceed the alkene atom in the SMILES string, the stereochem
+                        # information belongs to that atom
                         self.alkene_stereochem_dict[bonded_atoms[-2]] = stereochem_symbol
+
                 if not (bonded_atoms[1] in self.alkene_stereochem_dict.keys() or bonded_atoms[2] in self.alkene_stereochem_dict.keys()):
                     # not assigned stereochemistry
                     continue
+
                 for j, bonded_atom in enumerate(bonded_atoms[1:]):
                     if not bonded_atom in self.alkene_stereochem_dict.keys():
                         continue
+
                     if i == 0 and (bonded_atom > atom):
                         # F/C= is the same as C(\F)=
                         self.alkene_stereochem_dict[bonded_atom] = stereochem_swap[self.alkene_stereochem_dict[bonded_atom]]
+
                     if not bonded_atoms[2-j] in self.alkene_stereochem_dict.keys():
                         # may only have assigned stereochemistry for one atom
                         self.alkene_stereochem_dict[bonded_atoms[2-j]] = stereochem_swap[self.alkene_stereochem_dict[bonded_atom]]
                         break
+
                 bonded_atoms.sort()
                 # get the order of the stereochemistries of the atoms around the centre
                 stereochems = [self.alkene_stereochem_dict.get(bonded_atom, None) for bonded_atom in bonded_atoms]
                 none_index = stereochems.index(None)
                 if none_index == 2:
                     next_index = 0
+
                 else:
                     next_index = none_index + 1
+
                 if stereochems[next_index] == '/':
                     self.stereochem_dict[atom] = '@al'
+
                 else:
                     self.stereochem_dict[atom] = '@@al'
 
@@ -340,14 +417,18 @@ class SmilesParser:
         vectors = vectors_dict[self.stereochem_dict[central_atom]]
         self.stereocentres.append(central_atom)
         central_translation = -1 * self.atoms[central_atom].coord
+        # centre the central atom
         self.shift_atom(central_atom, central_translation)
         bonded_atoms = []
         for i, bond in enumerate(self.bonds):
+            # get the bonded atom in the order meant for the stereochemistry
             if central_atom in bond:
                 if bond[0] == central_atom:
                     bonded_atom = bond[1]
+
                 if bond[1] == central_atom:
                     bonded_atom = bond[0]
+
                 if self.stereochem_dict[central_atom] in ['@td', '@@td']:
                     # for tetrahedral centres, this is how the ordering is defined
                     if self.atoms[bonded_atom].label == 'H':
@@ -355,12 +436,16 @@ class SmilesParser:
                             bonded_atoms.insert(1, bonded_atom)
                         else:
                             bonded_atoms.insert(0, bonded_atom)
+
                     elif i in self.ring_closing_bond_list:
                         bonded_atoms.insert(0, bonded_atom)
+
                     else:
                         bonded_atoms.append(bonded_atom)
+
                 else:
                     bonded_atoms.append(bonded_atom)
+
         for i, bonded_atom in enumerate(bonded_atoms):
             if bonded_atom in self.stereocentres:
                 # don't want to lose the orientation around the old stereocentres, so rotate it into the right position
@@ -404,11 +489,13 @@ class SmilesParser:
         current_vector = self.atoms[central_rotating_atom].coord
         normed_current_vector = current_vector/np.linalg.norm(current_vector)
         axis = np.cross(normed_current_vector, new_vector)
+
         if np.allclose(axis, [0, 0, 0]):
             # if the vectors are antiparallel, the cross product is zero. rotate one vector a small amount to get a perpendicular vector to rotate about
             new_desired_vector = np.matmul(np.array(([0.996, -0.0872, 0], [0.0872, 0.996, 0], [0, 0, 1.0])), new_vector)
             axis = np.cross(new_desired_vector, normed_current_vector)
         angle = np.arccos(np.dot(new_vector, normed_current_vector))
+
         for atom in rotating_cluster:
             if not atom in [fixed_atom, central_rotating_atom]:
                 self.atoms[atom].rotate(axis, angle, current_vector)
@@ -426,13 +513,16 @@ class SmilesParser:
                     cluster_to_merge = i
                     merged_cluster = True
                     break
+
             if merged_cluster:
                 break
+
         if merged_cluster:
             for atom in new_cluster:
                 if not atom in self.stereocentre_clusters[cluster_to_merge]:
                     self.stereocentre_clusters[cluster_to_merge].append(atom)
             new_cluster = self.stereocentre_clusters[cluster_to_merge]
+
         else:
             self.stereocentre_clusters.append(new_cluster)
 
