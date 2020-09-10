ad903af85ca83953b9ed3c4ec5f4a172ce9e45c8
Unknown <ty13946@my.bristol.ac.uk>
2020-09-10 21:18:44 +0100

    Update documentation

--- doc/common/cope.py
+++ doc/common/cope.py
@@ -1,10 +1,9 @@
 from autode import Reactant, Product, Reaction
-from autode.input_output import xyz_file_to_atoms
 from autode.bond_rearrangement import BondRearrangement
 from autode.transition_states.locate_tss import get_ts
 
-r = Reactant(name='cope_r', atoms=xyz_file_to_atoms('cope_r.xyz'))
-p = Product(name='cope_p', atoms=xyz_file_to_atoms('cope_p.xyz'))
+r = Reactant('cope_r.xyz')
+p = Product('cope_p.xyz')
 reaction = Reaction(r, p, name='cope')
 
 # Define the bond rearrangement as tuples for each forming and breaking bond
