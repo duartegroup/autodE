7b2c4ab30d6801a4881e2dd67a6499ebad6c2ada
Unknown <ty13946@my.bristol.ac.uk>
2021-01-13 14:33:26 +0000

    Exception for bR < bP

--- tests/test_locate_tss.py
+++ tests/test_locate_tss.py
@@ -1,8 +1,13 @@
 import os
+import pytest
+import autode.exceptions as ex
+from autode.atoms import Atom
 from autode import Reactant, Product, Reaction
+from autode.species.complex import Complex
 from autode.reactions.reaction_types import Dissociation
 from autode.species.complex import get_complexes
-from autode.bond_rearrangement import get_bond_rearrangs
+from autode.bond_rearrangement import get_bond_rearrangs, BondRearrangement
+from autode.transition_states.locate_tss import get_ts
 from autode.transition_states.locate_tss import get_ts_guess_function_and_params
 
 
@@ -30,6 +35,31 @@ def test_one_to_three_dissociation():
     assert len(bond_rearrangement.bbonds) == 2
 
     # Ensure there is at least one bond function that could give the TS
-    ts_funcs_params = get_ts_guess_function_and_params(reaction,
-                                                       bond_rearrangement)
-    assert len(list(ts_funcs_params)) > 0
+    try:
+        ts_funcs_params = get_ts_guess_function_and_params(reaction,
+                                                           bond_rearrangement)
+        assert len(list(ts_funcs_params)) > 0
+
+    # Allow this function to be run with no avail EST methods
+    except ex.MethodUnavailable:
+        pass
+
+
+def test_more_forming_than_breaking():
+
+    h_a = Reactant(atoms=[Atom('H')], name='h_a')
+    h_b = Reactant(atoms=[Atom('H')], name='h_b')
+    h2_sep = Complex(h_a, h_b)
+
+    h2 = Product(atoms=[Atom('H'), Atom('H', x=1)], name='h2')
+
+    rxn = Reaction(h_a, h_b, h2)
+    bond_rearr = BondRearrangement(forming_bonds=[(0, 1)],
+                                   breaking_bonds=None)
+    assert bond_rearr.n_fbonds > bond_rearr.n_bbonds
+
+    # Number of bonds in the product needs to be the same or fewer than
+    # the reactant currently. Will need more get_ts_guess_function_and_params
+    # if this is to be supproted
+    with pytest.raises(NotImplementedError):
+        _ = get_ts(reaction=rxn, reactant=h2_sep, bond_rearr=bond_rearr)
