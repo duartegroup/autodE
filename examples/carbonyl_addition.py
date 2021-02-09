be213861deb26c99666a28974b8d515479a0cb00
Tom Young <39765193+t-young31@users.noreply.github.com>
2021-02-09 19:15:31 +0000

    Example correction (#29)

--- examples/carbonyl_addition.py
+++ examples/carbonyl_addition.py
@@ -2,11 +2,11 @@ import autode as ade
 
 ade.Config.n_cores = 8
 
-# For this reaction involving anionic CN- use a basis set with diffuse
-# functions
+# Use a basis set with diffuse functions this reaction involving anionic CN-
 ade.Config.ORCA.keywords.set_opt_basis_set('ma-def2-SVP')
 ade.Config.ORCA.keywords.sp.basis_set = 'ma-def2-TZVP'
 
-
+# Define a reaction as a single string, with reactants and products seperated
+# by '>>'
 reaction = ade.Reaction('CC(C)=O.[C-]#N>>CC([O-])(C#N)C', solvent_name='water')
 reaction.calculate_reaction_profile()
