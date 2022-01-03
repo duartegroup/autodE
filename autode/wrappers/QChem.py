Further tests ad Hessian extraction fix

autode/wrappers/QChem.py
@@ -110,7 +110,8 @@ class QChem(ElectronicStructureMethod):
                    for line in calc.output.file_lines)
 
     def optimisation_nearly_converged(self, calc) -> bool:
-        pass
+        # TODO: something better here
+        return False
 
     def get_final_atoms(self, calc) -> List[Atom]:
 
@@ -198,7 +199,7 @@ class QChem(ElectronicStructureMethod):
         if hess.shape != expected_shape:
             raise CouldNotGetProperty('hessian')
 
-        atom_masses = self._extract_atomic_masses(calc, units='au')
+        atom_masses = self._extract_atomic_masses(calc)
 
         # Un-mass weight
         mass_arr = np.repeat(atom_masses, repeats=3, axis=np.newaxis)
@@ -208,11 +209,7 @@ class QChem(ElectronicStructureMethod):
         return hess / Constants.a0_to_ang ** 2
 
     @staticmethod
-    def _extract_atomic_masses(calc, units):
-
-        if units != 'au':
-            raise NotImplementedError('Atom mass extraction from QChem output '
-                                      'is only supported in atomic units')
+    def _extract_atomic_masses(calc) -> np.ndarray:
 
         masses = []
         for line in calc.output.file_lines:
@@ -221,11 +218,11 @@ class QChem(ElectronicStructureMethod):
                 # e.g.
                 #   Atom    1 Element O  Has Mass   15.99491
 
-                mass = float(line.split()[-1]) * Constants.amu_to_me
+                mass = float(line.split()[-1])
                 masses.append(mass)
 
         # Only return the final n_atoms masses
-        return masses[-calc.molecule.n_atoms:]
+        return np.array(masses[-calc.molecule.n_atoms:])
 
     @staticmethod
     def _extract_mass_weighted_hessian(calc) -> np.ndarray:
@@ -256,11 +253,15 @@ class QChem(ElectronicStructureMethod):
                 try:
                     start_idx = end_idx + 2
                     end_idx = start_idx + three_n_atoms
+                    lines_slice = lines[start_idx:end_idx]
 
-                    for j, _l in enumerate(lines[start_idx:end_idx]):
+                    if len(lines_slice) == 0:
+                        raise AssertionError
+
+                    for j, _l in enumerate(lines_slice):
                         hess[j] += [float(val) for val in _l.split()]
 
-                except (TypeError, ValueError):
+                except (TypeError, ValueError, AssertionError):
                     raise CouldNotGetProperty('hessian')
 
         return np.array(hess)
@@ -333,7 +334,7 @@ class QChem(ElectronicStructureMethod):
                     self.write(f'dft_d {word.qchem}')
 
                 elif isinstance(word, kws.MaxOptCycles):
-                    self.write(f'geom_opt_max_cycle {word}')
+                    self.write(f'geom_opt_max_cycles {word}')
 
                 elif isinstance(word, kws.ECP):
                     self._write_ecp(word, molecule=molecule)

tests/test_qchem.py
@@ -286,7 +286,11 @@ def test_gradient_extraction_h2o():
     assert grad.shape == (3, 3)
 
     # The minimum should have a gradient close to zero
-    assert np.allclose(grad, np.zeros(shape=(3, 3)), atol=1E-4)
+    assert np.allclose(grad, np.zeros(shape=(3, 3)), atol=1E-4)\
+
+    # also for this calculation the optimisation has converged
+    assert method.optimisation_converged(calc)
+    assert not method.optimisation_nearly_converged(calc)
 
 
 @work_in_zipped_dir(qchem_data_zip_path)
@@ -312,5 +316,38 @@ def test_h2o_hessian_extraction():
     hess = method.get_hessian(calc)
     assert hess.shape == (9, 9)
 
-    print(hess)
+    # Check the first element is close to that of an ORCA-derived equiv.
+    # in Ha / Å-2
+    assert np.isclose(hess[0, 0],
+                      2.31423829e+00,
+                      atol=0.1)
 
+
+def test_broken_hessian_extraction():
+    calc = _broken_output_calc()
+
+    with pytest.raises(CalculationException):
+        _ = method.get_hessian(calc)
+
+    calc = _custom_output_calc('some', 'output', 'then',
+                               'Mass-Weighted Hessian Matrix', 'X')
+
+    with pytest.raises(CalculationException):
+        _ = method.get_hessian(calc)
+
+    if os.path.exists('tmp.out'):
+        os.remove('tmp.out')
+
+
+@work_in_zipped_dir(qchem_data_zip_path)
+def test_calc_terminated_normally_max_opt_cycles():
+
+    # h2o.optimise(method=ade.methods.QChem(),
+    # keywords=['method pbe', 'basis def2-SVP', 'geom_opt_max_cycle 2'])
+
+    calc = _blank_calc()
+    calc.output.filename = 'H2O_opt_max_2_cycles.out'
+    assert calc.output.exists
+
+    # Even with a 'fatal error' in the output the calculation was ok
+    assert calc.terminated_normally