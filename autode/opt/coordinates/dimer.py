Add correct units to dimer coordinates

autode/opt/coordinates/dimer.py
@@ -7,7 +7,8 @@ from enum import IntEnum, unique
 from typing import Union, Sequence, Optional
 from autode.opt.coordinates.base import OptCoordinates
 from autode.log import logger
-from autode.values import Angle, Distance
+from autode.values import Angle, MWDistance
+from autode.units import ang_amu_half
 
 
 @unique
@@ -23,9 +24,11 @@ class DimerCoordinates(OptCoordinates):
     """Mass weighted Cartesian coordinates for two points in space forming
     a dimer, such that the midpoint is close to a first order saddle point"""
 
+    implemented_units = [ang_amu_half]
+
     def __new__(cls,
                 input_array: Union[Sequence, np.ndarray],
-                units:       Union[str, 'autode.units.Unit'] = 'Å'
+                units:       Union[str, 'autode.units.Unit'] = 'Å amu^1/2'
                 ) -> 'OptCoordinates':
         """New instance of these coordinates"""
 
@@ -34,9 +37,9 @@ class DimerCoordinates(OptCoordinates):
         if arr.ndim != 2 or arr.shape[0] != 3:
             raise ValueError('Dimer coordinates must ')
 
-        arr._e = None             # Energy
-        arr.dist = Distance(0.0)  # Translation distance
-        arr._phi = Angle(0.0)     # Rotation amount
+        arr._e = None                             # Energy
+        arr._dist = MWDistance(0.0, 'Å amu^1/2')  # Translation distance
+        arr._phi = Angle(0.0, 'radians')          # Rotation amount
 
         """
         Compared to standard Cartesian coordinates these arrays have and
@@ -52,7 +55,7 @@ class DimerCoordinates(OptCoordinates):
     def __array_finalize__(self, obj: 'OptCoordinates') -> None:
         """See https://numpy.org/doc/stable/user/basics.subclassing.html"""
 
-        for attr in ('units', '_e', '_g', '_h', 'dist', '_phi', 'masses'):
+        for attr in ('units', '_e', '_g', '_h', '_dist', '_phi', 'masses'):
             self.__dict__[attr] = getattr(obj, attr, None)
 
         return None
@@ -218,20 +221,32 @@ class DimerCoordinates(OptCoordinates):
     @property
     def delta(self) -> float:
         """Distance between the dimer point, Δ"""
-        x = np.linalg.norm(self.x1 - self.x2) / 2.0
-        return Distance(x, units=self.units)
+        return MWDistance(np.linalg.norm(self.x1 - self.x2) / 2.0,
+                          units=self.units)
 
     @property
     def phi(self) -> Angle:
-        """Angle that the dimer was rotated by from it's last position"""
+        """Angle that the dimer was rotated by from its last position"""
         return self._phi
 
     @phi.setter
     def phi(self, value: Angle):
         if not isinstance(value, Angle):
-            raise ValueError('phi must be an autode.values.Value instance')
+            raise ValueError('phi must be an autode.values.Angle instance')
 
         self._phi = value
+
+    @property
+    def dist(self) -> MWDistance:
+        """Distance that the dimer was translated by from its last position"""
+        return self._dist
+
+    @dist.setter
+    def dist(self, value: MWDistance):
+        if not isinstance(value, MWDistance):
+            raise ValueError('dist must be an autode.values.Distance instance')
+
+        self._dist = value
 
     @property
     def did_rotation(self):

autode/opt/optimisers/dimer.py
@@ -15,7 +15,7 @@ from typing import Optional
 from enum import Enum
 from autode.calculation import Calculation
 from autode.log import logger
-from autode.values import GradientNorm, Angle, Distance
+from autode.values import GradientNorm, Angle, MWDistance
 from autode.opt.optimisers.base import Optimiser
 from autode.opt.coordinates.dimer import DimerCoordinates, DimerPoint
 
@@ -27,9 +27,10 @@ class Dimer(Optimiser):
                  maxiter:         int,
                  coords:          DimerCoordinates,
                  ratio_rot_iters: int = 10,
-                 gtol:            GradientNorm = GradientNorm(1E-3, units='Ha Å-1'),
-                 phi_tol:         Angle = Angle(5.0, units='°'),
-                 init_alpha:      Distance = Distance(0.3, units='Å')
+                 gtol:            GradientNorm = GradientNorm(1E-3, 'Ha Å-1'),
+                 trns_tol:        MWDistance = MWDistance(1E-3, 'Å amu^1/2'),
+                 phi_tol:         Angle = Angle(5.0, '°'),
+                 init_alpha:      MWDistance = MWDistance(0.3, 'Å amu^1/2')
                  ):
         """
         Dimer optimiser
@@ -47,6 +48,9 @@ class Dimer(Optimiser):
 
             gtol: Tolerance on the gradient at the midpoint for convergence
 
+            trns_tol: Tolerance on the minimum root mean square translation
+                      distance, below which convergence is signaled
+
             phi_tol: Tolerance on the rotation angle below which rotation is
                      not performed
 
@@ -57,6 +61,7 @@ class Dimer(Optimiser):
 
         self._ratio_rot_iters = ratio_rot_iters
         self.gtol = gtol
+        self.trns_tol = trns_tol
         self.phi_tol = phi_tol
         self.init_alpha = init_alpha
 
@@ -158,39 +163,34 @@ class Dimer(Optimiser):
         trns_iters = [c for c in self._history if c.did_translation]
 
         if len(trns_iters) < 2:
-            step_size = float(self.init_alpha.to("Å"))
-
+            step_size = float(self.init_alpha)
             logger.info(f'Did not have two previous translation step, guessing'
-                        f' γ = {step_size} Å')
+                        f' α = {step_size} Å')
 
         else:
             prev_trns_iter = trns_iters[-2]
             logger.info(f'Did {len(trns_iters)} previous translations, can '
-                        f'calculate γ')
+                        f'calculate α using the Barzilai–Borwein method')
 
-            # Barzilai–Borwein method for the step size
             step_size = (np.abs(np.dot((x0 - prev_trns_iter.x0),
                                        (self._coords.f_t - prev_trns_iter.f_t)))
                          / np.linalg.norm(self._coords.f_t - prev_trns_iter.f_t)**2)
 
-        # TODO: Read where this comes from
-        # step_size = float(self.init_alpha.to("Å"))
-
         delta_x = step_size * self._coords.f_t
-        length = np.linalg.norm(delta_x) / len(x0)
+        trns_rms = MWDistance(np.sqrt(np.mean(np.square(delta_x))))
 
-        if length < Distance(1E-4, units='Å'):
-            logger.info('Step length small than tolerance')
+        if trns_rms < self.trns_tol:
+            logger.info(f'Step length small than tolerance {self.trns_tol}')
             return _StepResult.skipped_translation
 
-        logger.info(f'Translating by {length:.4f} Å per coordinate')
+        logger.info(f'Translating by ~{trns_rms:.4f} per coordinate')
 
         coords = self._coords.copy()
         coords += delta_x
 
         self._coords = coords
         self._coords.phi = Angle(0.0)    # Did not rotation
-        self._coords.dist = length       # but did translate
+        self._coords.dist = trns_rms     # but did translate
 
         if update_g0:
             self._update_gradient_at(DimerPoint.midpoint)
@@ -307,14 +307,14 @@ class Dimer(Optimiser):
 
         if max_step_c > self.init_alpha:
             logger.warning(f'Step size ({max_step_c}) was above the tolerance'
-                           f' {self.init_alpha.to("Å")} Å. Scaling down')
+                           f' {self.init_alpha} Å amu^1/2. Scaling down')
             return self._rotate_coords(phi=phi/2, update_g1=update_g1)
 
         self._coords = self._coords.copy()
         self._coords.x1 = x0 + delta
         self._coords.x2 = x0 - delta
 
-        self._coords.dist = Distance(0.0)
+        self._coords.dist = MWDistance(0.0)
         self._coords.phi = phi
 
         # Midpoint has not moved so it's gradient its retained
@@ -328,7 +328,7 @@ class Dimer(Optimiser):
 
         logger.info(f'Rotated coordinates, now have |g1 - g0| = '
                     f'{np.linalg.norm(self._coords.g1 - self._coords.g0):.4f}.'
-                    f' ∆ = {self._coords.delta.to("Å"):.3f} Å')
+                    f' ∆ = {self._coords.delta:.3f}')
         return None
 
     def _optimise_rotation(self):

tests/test_opt/test_dimer.py
@@ -3,7 +3,7 @@ import numpy as np
 from autode.atoms import Atom
 from autode.species.molecule import Molecule
 from autode.methods import XTB
-from autode.values import Distance
+from autode.values import MWDistance
 from autode.opt.coordinates.dimer import DimerCoordinates, DimerPoint
 from autode.opt.optimisers.dimer import Dimer
 from ..testutils import requires_with_working_xtb_install
@@ -178,7 +178,7 @@ def test_dimer_2d():
 
     dimer = Dimer2D(maxiter=100,
                     coords=DimerCoordinates(arr),
-                    init_alpha=Distance(0.5))
+                    init_alpha=MWDistance(0.5))
 
     # Check the midpoint of the dimer is positioned correctly
     assert np.allclose(dimer._coords.x0,