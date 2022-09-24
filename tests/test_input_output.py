cdea1cbfcf9fa83ed01be3e3414d25025e99e8bc
tom <tom.young.316@gmail.com>
2022-09-24 14:50:22 +0100

    Add input test

--- tests/test_input_output.py
+++ tests/test_input_output.py
@@ -1,6 +1,9 @@
-from autode.input_output import xyz_file_to_atoms, atoms_to_xyz_file
+import numpy as np
+
+from autode.input_output import xyz_file_to_atoms, atoms_to_xyz_file, xyz_file_to_molecules
 from autode.exceptions import XYZfileDidNotExist, XYZfileWrongFormat
 from autode.atoms import Atom, Atoms
+from autode.utils import work_in_tmp_dir
 from . import testutils
 import pytest
 import os
@@ -29,6 +32,7 @@ def test_xyz_file_to_atoms():
         xyz_file_to_atoms(filename='wrong_ext.mol')
 
 
+@work_in_tmp_dir()
 def test_xyz_file_incorrect_n_atoms():
 
     with open('test.xyz', 'w') as xyz_file:
@@ -40,9 +44,8 @@ def test_xyz_file_incorrect_n_atoms():
     with pytest.raises(XYZfileWrongFormat):
         _ = xyz_file_to_atoms('test.xyz')
 
-    os.remove('test.xyz')
 
-
+@work_in_tmp_dir()
 def test_xyz_file_incorrect_first_line():
     with open('test.xyz', 'w') as xyz_file:
         print('XXX',
@@ -53,9 +56,8 @@ def test_xyz_file_incorrect_first_line():
     with pytest.raises(XYZfileWrongFormat):
         _ = xyz_file_to_atoms('test.xyz')
 
-    os.remove('test.xyz')
 
-
+@work_in_tmp_dir()
 def test_making_xyz_file():
 
     atoms = [Atom('H'), Atom('H')]
@@ -79,4 +81,40 @@ def test_making_xyz_file():
         # Needs .xyz extension
         atoms_to_xyz_file(atoms, filename='test')
 
-    os.remove('test.xyz')
+
+def _print_xyz_file_two_h_molecules(title_line: str ="title line") -> None:
+
+    xyz_file_string = (
+        "1\n"
+        f"{title_line}\n"
+        "H 0.0 0.0 0.0"
+    )
+
+    with open("tmp.xyz", "w") as file:
+        print(xyz_file_string,
+              xyz_file_string,
+              sep="\n", file=file)
+
+
+@work_in_tmp_dir()
+def test_reading_multi_molecule_file_no_defined_values():
+
+    _print_xyz_file_two_h_molecules()
+    molecules = xyz_file_to_molecules("tmp.xyz")
+    assert len(molecules) == 2
+    m = molecules[0]
+
+    # Molecules should be generated with sensible defaults
+    assert m.energy is None
+    assert m.charge == 0
+    assert m.mult == 1
+
+
+@work_in_tmp_dir()
+def test_reading_multi_molecule_file_defined_charge_mult_energy():
+
+    _print_xyz_file_two_h_molecules(title_line="E = -0.5 charge = 0 mult = 2")
+    m = xyz_file_to_molecules("tmp.xyz")[0]
+    assert m.energy is not None and np.isclose(m.energy, -0.5)
+    assert m.charge == 0
+    assert m.mult == 2
