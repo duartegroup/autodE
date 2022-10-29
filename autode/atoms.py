290a2b951153f2ebf14c8323062b441c7b25a77d
tom <tom.young.316@gmail.com>
2022-10-29 17:10:23 +0100

    -

--- autode/atoms.py
+++ autode/atoms.py
@@ -1242,7 +1242,7 @@ class PeriodicTable:
          ['Rb', 'Sr',  'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te',  'I', 'Xe'],
          ['Cs', 'Ba',   '', 'Hf', 'Ta',  'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn'],
          ['Fr', 'Ra',   '', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']],
-        type=str
+        dtype=str
     )
     # fmt: on
 
