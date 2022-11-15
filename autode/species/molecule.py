b8ce7afa6b5025fabf188936b40139a9037bfa5e
tom <tom.young.316@gmail.com>
2022-11-15 18:53:44 +0000

    Remove non-required warning

--- autode/species/molecule.py
+++ autode/species/molecule.py
@@ -87,20 +87,13 @@ class Molecule(Species):
             smiles (str):
         """
         at_strings = re.findall(r"\[.*?]", smiles)
-        init_charge, init_mult = self._charge, self._mult
+        init_charge = self._charge
 
         if any(metal in string for metal in metals for string in at_strings):
             init_smiles(self, smiles)
 
         else:
             init_organic_smiles(self, smiles)
-
-        if not _is_default_mult(init_mult) and init_mult != self._mult:
-            logger.warning(
-                "Specified multiplicity did not match SMILES "
-                "calculated value - using the former"
-            )
-            self._mult = init_mult
 
         if not _is_default_charge(init_charge) and init_charge != self._charge:
             raise ValueError(
