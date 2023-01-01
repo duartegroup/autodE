30ac6c4151f87481116829ba9def4c7d55a29ee8
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2023-01-01 09:16:39 +0530

    minor fix for bash ci helper script

--- install_xtb_ci.sh
+++ install_xtb_ci.sh
@@ -2,7 +2,7 @@
 # please note that the version for windows is hardcoded and needs to be changed
 # if a newer version of xTB is released.
 if [[ "$OSTYPE" == "msys" ]]; then
-  curl https://github.com/grimme-lab/xtb/releases/download/v6.5.1/xtb-6.5.1-windows-x86_64.zip --output xtb_win.zip
+  curl -kLSs https://github.com/grimme-lab/xtb/releases/download/v6.5.1/xtb-6.5.1-windows-x86_64.zip --output xtb_win.zip
   unzip xtb_win.zip
   export XTBPATH=./xtb-6.5.1/share/xtb
   export PATH="./xtb-6.5.1/bin:$PATH"
