d4669473390878a4a7d05096a95d5c0ec7ada119
shoubhikraj <shoubhikrajmaiti@gmail.com>
2023-02-13 21:58:47 +0000

    imagepair tests

--- tests/test_bracket/test_imgpair.py
+++ tests/test_bracket/test_imgpair.py
@@ -4,19 +4,20 @@ import pytest
 from autode.bracket.imagepair import BaseImagePair
 from autode import Molecule
 from autode.utils import work_in
+from ..testutils import requires_with_working_xtb_install
 
 
 @work_in("data")
 def test_imgpair_alignment():
     # with same molecule, alignment should produce same coordinates
-    mol1 = Molecule('sn2_rct.xyz', charge=-1)
-    mol2 = Molecule('sn2_rct_rotated.xyz', charge=-1)
+    mol1 = Molecule('da_reactant.xyz')
+    mol2 = Molecule('da_reactant_rotated.xyz')
     imgpair = BaseImagePair(mol1, mol2)
 
     # alignment happens on init
     new_mol1, new_mol2 = imgpair._left_image, imgpair._right_image
     # left image should have been rotated to align perfectly
-    assert np.allclose(new_mol1.coordinates, new_mol2.coordinates, atol=1.e-6)
+    assert np.allclose(new_mol1.coordinates, new_mol2.coordinates, atol=1.e-5)
     # right image should be translated only, i.e. all difference same
     diff = mol2.coordinates - new_mol2.coordinates
     assert np.isclose(diff, diff[0]).all()
@@ -28,19 +29,20 @@ def test_imgpair_alignment():
 
 @work_in("data")
 def test_imgpair_sanity_check():
-    mol1 = Molecule('sn2_rct.xyz', charge=-1)
-    mol2 = Molecule('sn2_rct_rotated.xyz', charge=-1)
+    mol1 = Molecule('da_reactant.xyz')
+    mol2 = Molecule('da_reactant_rotated.xyz')
     mol3 = Molecule(smiles='CCCO')
+    mol4 = Molecule('da_reactant_shuffled.xyz')
 
     # different mol would raise Error
     with pytest.raises(ValueError):
         _ = BaseImagePair(mol1, mol3)
 
     # different charge would raise Error
-    mol1.charge = -3
+    mol1.charge = -2
     with pytest.raises(ValueError):
         _ = BaseImagePair(mol1, mol2)
-    mol1.charge = -1
+    mol1.charge = 0
 
     # different multiplicity would also raise Error
     mol1.mult = 3
@@ -49,3 +51,10 @@ def test_imgpair_sanity_check():
     mol1.mult = 1
 
     # different atom order should also raise Error
+    with pytest.raises(ValueError):
+        _ = BaseImagePair(mol1, mol4)
+
+
+@requires_with_working_xtb_install
+def test_set_energy_gradient():
+    pass
