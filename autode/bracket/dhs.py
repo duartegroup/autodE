21aa4b8f810615e33f887415294dbb6246111b7d
shoubhikraj <shoubhikrajmaiti@gmail.com>
2023-02-13 16:17:15 +0000

    imagepair.py: add single point energy calcs

--- autode/bracket/dhs.py
+++ autode/bracket/dhs.py
@@ -116,6 +116,9 @@ class DHS:
                               decreased in each DHS step
             dist_tol: The distance tolerance at which DHS will
                       stop, values <0.5 Angstrom are not recommended!
+            optimiser: The optimiser to use for minimising after
+                       after the DHS step, either 'CG' (conjugate
+                       gradients) or 'BFGS'
         """
         self.imgpair = DHSImagePair(initial_species, final_species)
         self._species = initial_species.copy()  # just hold the species
