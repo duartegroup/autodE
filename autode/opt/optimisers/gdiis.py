248b619e84cac603a249479681a72f83ce0a8b1c
shoubhikraj <shoubhikrajmaiti@gmail.com>
2023-02-28 15:39:37 +0000

    gdiis updates

--- autode/opt/optimisers/gdiis.py
+++ autode/opt/optimisers/gdiis.py
@@ -18,7 +18,7 @@ class GDIIS:
         self,
         history: _OptimiserHistory,
         max_vecs=5,
-        use_last_hessian=True,
+        use_hessian=True,
         check_stability=True,
         trust=None,
     ):
@@ -36,12 +36,12 @@ class GDIIS:
 
         # todo warn if max_vec larger than history
         self._max_vec = int(max_vecs)
-        self._space = history[: self._max_vec]
+        self._space = history[::-1][:self._max_vec]  # reverse to get the latest first
 
-        if use_last_hessian:
-            self._H = history[-1].h
+        if use_hessian:
+            self._H, self._ref_step = self.get_qnr_step()
         else:
-            self._H = None
+            self._H, self._ref_step = None, None
 
         self._check_stable = bool(check_stability)
         self._trust = trust
@@ -50,17 +50,16 @@ class GDIIS:
         n = len(self._space)
 
         if self._H is None:
-            self._H = np.eye(n)
+            self._H = np.eye(len(self._space[0].g))
 
-        orig_err_vecs = [-np.matmul(self._H, coord.g) for coord in self._space]
-        orig_err_vecs = orig_err_vecs[::-1]  # reverse to get the latest first
+        err_vecs = [-np.matmul(self._H, coord.g) for coord in self._space]
 
         # rescale error vectors by dividing by the smallest error vector norm
         # criteria (d), page 13, Phys. Chem. Chem. Phys., 2002, 4, 11–15
         scale_fac = min(
-            [np.linalg.norm(err_vec) for err_vec in orig_err_vecs]
+            [np.linalg.norm(err_vec) for err_vec in err_vecs]
         )
-        err_vecs = [err_vec / scale_fac for err_vec in orig_err_vecs]
+        err_vecs = [err_vec / scale_fac for err_vec in err_vecs]
 
         gdiis_coeffs = None
         for num_vec in range(2, min(self._max_vec, n) + 1):
@@ -75,7 +74,7 @@ class GDIIS:
                 coeffs = np.linalg.solve(a_mat, np.ones(num_vec))
             except np.linalg.LinAlgError:
                 break
-            # check for singularity of A matrix nonetheless
+            # check for singularity of A matrix always
             coeff_size = np.linalg.norm(coeffs)
             if coeff_size >= 1.0e8:
                 break
@@ -84,8 +83,40 @@ class GDIIS:
             if not self._check_stable:
                 gdiis_coeffs = scaled_coeffs
                 continue
-
+            pred_coord = self._get_gdiis_coord(scaled_coeffs)
             is_angle_valid = self._check_against_ref_step()
+
+    def _get_gdiis_coord(self, coeffs):
+        """Get predicted coordinate for GDIIS"""
+        # todo do we need original error vectors or new vectors
+        pass
+
+    def _get_qnr_step(self):
+        # try RFO step first
+        hess = self._space[-1].h
+        grad = self._space[-1].g
+        assert hess is not None
+        h_n = hess.shape[0]
+
+        aug_h = np.zeros(shape=(h_n+1, h_n+1))
+
+        aug_h[:h_n, :h_n] = hess
+        aug_h[-1, :h_n] = grad
+        aug_h[:h_n, -1] = grad
+
+        aug_h_lmda, aug_h_v = np.linalg.eigh(aug_h)
+        mode = np.where(np.abs(aug_h_lmda) > 1.e-15)[0][0]
+
+        step = aug_h_v[:-1, mode] / aug_h_v[-1, mode]
+
+        # todo if step is not within trust radius then do
+        # qa
+        h_eff = hess - aug_h_lmda[mode] * np.eye(h_n)
+        # todo check this formula
+        return h_eff, step.flatten()
+
+    def _check_against_ref_step(self, gdiis_step):
+        pass
 
 
 
