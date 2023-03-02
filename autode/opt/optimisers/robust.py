5586fbed00be8d9e83b743fc095a73a21eb5ff07
Shoubhik Maiti <shoubhikrajmaiti@gmail.com>
2023-03-02 22:37:51 +0000

    small changes

--- autode/opt/optimisers/robust.py
+++ autode/opt/optimisers/robust.py
@@ -5,3 +5,17 @@ implemented in common QM packages)
 """
 
 import numpy as np
+
+from autode.opt.optimisers import RFOptimiser
+
+
+class RobustOptimiser(RFOptimiser):
+    def __init__(
+        self,
+        *args,
+        init_trust: float = 0.1,
+        min_trust: float = 0.01,
+        max_trust: float = 0.3,
+        **kwargs,
+    ):
+        super().__init__(*args, init_alpha=init_trust, **kwargs)
