71940857c35308b5f88890aeef909a01157c4b50
Shoubhik Maiti <shoubhikrajmaiti@gmail.com>
2023-03-06 09:34:27 +0000

    fix qa step to use cartesian step size

--- autode/opt/optimisers/robust.py
+++ autode/opt/optimisers/robust.py
@@ -32,142 +32,134 @@ class RobustOptimiser(RFOptimiser):
         self._coords.h = self._updated_h()
         h_n = self._coords.h.shape[0]
 
-        rfo_lmda = _get_rfo_minimise_lambda(self._coords.h, self._coords.g)
-        h_eff = self._coords.h - rfo_lmda * np.eye(h_n)
-        rfo_step = -np.linalg.inv(h_eff) @ self._coords.g
+        rfo_h_eff = self._get_rfo_minimise_h_eff()
+        rfo_step = -np.linalg.inv(rfo_h_eff) @ self._coords.g
+        rfo_step_size = self._get_cart_step_size_from_step(rfo_step)
 
-        if self._step_scale_factor_within_trust(rfo_step) > 0.99:
+        if rfo_step_size < self.alpha:
+            print(f"Taking a pure RFO step of size: {rfo_step_size:.3f}")
             self._coords = self._coords + rfo_step
 
         else:
             try:
-                qa_lmda = _get_qa_minimise_lambda(
-                    self._coords.h, self._coords.g, self.alpha
+                qa_h_eff = self._get_qa_minimise_h_eff()
+                qa_step = -np.linalg.inv(qa_h_eff) @ self._coords.g
+                print(
+                    f"Taking a QA step exactly at trust"
+                    f" radius: {self.alpha}"
                 )
-                h_eff = self._coords.h - qa_lmda * np.eye(h_n)
-                qa_step = -np.linalg.inv(h_eff) @ self._coords.g
                 self._coords = self._coords + qa_step
 
             except OptimiserStepError:
                 # if QA failed, take scaled RFO step
-                pass
+                scaled_step = rfo_step * (self.alpha / rfo_step_size)
+                # step size is in Cartesian but scaling should work
+                step_size = self._get_cart_step_size_from_step(scaled_step)
+                print(
+                    f"Taking a scaled RFO step approximately within "
+                    f"trust radius: {step_size:.3f}"
+                )
+                self._coords = self._coords + scaled_step
 
-    def _step_scale_factor_within_trust(self, step: np.ndarray) -> float:
+        return None
+
+    def _get_cart_step_size_from_step(self, step: np.ndarray) -> float:
         """
-        Returns the scale factor that must be applied to the step
-        to have it within the trust radius. If the calculated step
-        is smaller than trust radius, return 1.0 i.e. no scaling
+        Obtains the Cartesian step size given a step in the current
+        coordinate system (e.g., Delocalised Internal Coordinates)
 
         Args:
-            step (np.ndarray): The step calculated
+            step (np.ndarray): The step in the current coordinates
 
         Returns:
-            (float): The scale factor
+            (float): The step size in Cartesian
         """
         new_coords = self._coords + step
-        cartesian_delta = new_coords.to("cart") - self._coords.to("cart")
-        step_size = np.linalg.norm(cartesian_delta)
+        cart_delta = new_coords.to("cart") - self._coords.to("cart")
+        return float(np.linalg.norm(cart_delta))
 
-        # return the scale factor that should be applied
-        if step_size <= self.alpha:
-            # no scaling if step is smaller
-            return 1.0
+    def _get_rfo_minimise_h_eff(self) -> np.ndarray:
+        """
+        Using current Hessian and gradient, obtain the level-shifted
+        Hessian that would provide a minimising RFO step
 
-        elif step_size > self.alpha:
-            return self.alpha / step_size
+        Returns:
+            (np.ndarray): The level-shifted effective Hessian
+        """
+        h_n = self._coords.h.shape[0]
 
+        # form the augmented Hessian
+        aug_h = np.zeros(shape=(h_n + 1, h_n + 1))
+        aug_h[:h_n, :h_n] = self._coords.h
+        aug_h[-1, :h_n] = self._coords.g
+        aug_h[:h_n, -1] = self._coords.g
 
-def _get_rfo_minimise_lambda(
-    hessian: np.ndarray, gradient: np.ndarray
-) -> float:
-    """
-    Using current Hessian and gradient, obtain the lambda
-    (Hessian shift parameter) that would give a minimising
-    step, using RFO-like method
+        aug_h_lmda, aug_h_v = np.linalg.eigh(aug_h)
 
-    Args:
-        hessian (np.ndarray):
-        gradient (np.ndarray):
-    Returns:
-        (float): lambda that gives minimising RFO step
-    """
-    h_n = hessian.shape[0]
+        # RFO step uses the lowest non-zero eigenvalue
+        mode = np.where(np.abs(aug_h_lmda) > 1.0e-15)[0][0]
+        lmda = aug_h_lmda[mode]
 
-    # form the augmented Hessian
-    aug_h = np.zeros(shape=(h_n + 1, h_n + 1), dtype=np.float64)
-    aug_h[:h_n, :h_n] = hessian
-    aug_h[-1, :h_n] = gradient
-    aug_h[:h_n, -1] = gradient
+        # effective hessian = H - lambda * I
+        return self._coords.h - lmda * np.eye(h_n)
 
-    aug_h_lmda, aug_h_v = np.linalg.eigh(aug_h)
+    def _get_qa_minimise_h_eff(self) -> np.ndarray:
+        """
+        Using current Hessian and gradient, get the level-shifted Hessian
+        for a minimising step, whose magnitude (norm) is equal to the
+        trust radius (Quadratic Approximation step).
+        Described in J. Golab, D. L. Yeager, Chem. Phys., 78, 1983, 175-199
 
-    # RFO step uses the lowest non-zero eigenvalue
-    mode = np.where(np.abs(aug_h_lmda) > 1.0e-10)[0][0]
-    lmda = aug_h_lmda[mode]
+        Returns:
+            (np.ndarray): The level-shifted Hessian for QA step
+        """
+        # this function is expensive to call
+        from scipy.optimize import root_scalar
 
-    return lmda
+        h_n = self._coords.h.shape[0]
+        h_eigvals = np.linalg.eigvalsh(self._coords.h)
+        first_mode = np.where(np.abs(h_eigvals) > 1.0e-15)[0][0]
+        first_b = h_eigvals[first_mode]  # first non-zero eigenvalue of H
 
+        def step_length_error(lmda):
+            shifted_h = self._coords.h - lmda * np.eye(
+                h_n
+            )  # level-shifted hessian
+            inv_shifted_h = np.linalg.inv(shifted_h)
+            step = -inv_shifted_h @ self._coords.g
+            step_size = self._get_cart_step_size_from_step(step)
+            return step_size - self.alpha
 
-def _get_qa_minimise_lambda(
-    hessian: np.ndarray, gradient: np.ndarray, trust: float
-) -> float:
-    """
-    Using current Hessian and gradient, get the lambda (Hessian
-    shift parameter) for a minimising step, whose magnitude (norm)
-    is equal to the trust radius (Quadratic Approximation step).
-    Described in J. Golab, D. L. Yeager, Chem. Phys., 78, 1983, 175-199
+        # The value of shift parameter lambda must lie within (-infinity, first_b)
+        # Need to find the roots of the 1D function step_length_error
+        l_plus = 1.0
+        for _ in range(1000):
+            err = step_length_error(first_b - l_plus)
+            if err > 0.0:  # found location where f(x) > 0
+                break
+            l_plus *= 0.5
+        else:  # if loop didn't break
+            raise OptimiserStepError("Unable to find lambda where error > 0")
 
-    Args:
-        hessian (np.ndarray):
-        gradient (np.ndarray):
-        trust (float):
+        l_minus = l_plus + 1.0
+        for _ in range(1000):
+            err = step_length_error(first_b - l_minus)
+            if err < 0.0:  # found location where f(x) < 0
+                break
+            l_minus += 1.0  # reduce lambda by 1.0
+        else:
+            raise OptimiserStepError("Unable to find lambda where error < 0")
 
-    Returns:
-        (float): lambda that gives the QA minimising step
-    """
-    from scipy.optimize import root_scalar
+        # Use scipy's root finder
+        res = root_scalar(
+            step_length_error,
+            method="brentq",
+            bracket=[first_b - l_minus, first_b - l_plus],
+            maxiter=200,
+        )
 
-    n = hessian.shape[0]
-    h_eigvals = np.linalg.eigvalsh(hessian)
-    first_mode = np.where(np.abs(h_eigvals) > 1.0e-10)[0][0]
-    first_b = h_eigvals[first_mode]  # first non-zero eigenvalue of H
+        if not res.converged:
+            raise OptimiserStepError("Unable to find root of error function")
 
-    def step_length_error(lmda):
-        shifted_h = hessian - lmda * np.eye(n)  # level-shifted hessian
-        inv_shifted_h = np.linalg.inv(shifted_h)
-        step = -inv_shifted_h @ gradient.reshape(-1, 1)
-        return np.linalg.norm(step) - trust
-
-    # The value of shift parameter lambda must lie within (-infinity, first_b)
-    # Need to find the roots of the 1D function step_length_error
-    l_plus = 1.0
-    for _ in range(1000):
-        err = step_length_error(first_b - l_plus)
-        if err > 0.0:  # found location where f(x) > 0
-            break
-        l_plus *= 0.5
-    else:  # if loop didn't break
-        raise OptimiserStepError("Unable to find lambda where error > 0")
-
-    l_minus = l_plus + 1.0
-    for _ in range(1000):
-        err = step_length_error(first_b - l_minus)
-        if err < 0.0:  # found location where f(x) < 0
-            break
-        l_minus += 1.0  # reduce lambda by 1.0
-    else:
-        raise OptimiserStepError("Unable to find lambda where error < 0")
-
-    # Use scipy's root finder
-    res = root_scalar(
-        step_length_error,
-        method="brentq",
-        bracket=[first_b - l_minus, first_b - l_plus],
-        maxiter=200,
-    )
-
-    if not res.converged:
-        raise OptimiserStepError("Unable to find root of error function")
-
-    # this is final lambda
-    return res.root
+        # use the final lambda to construct level-shifted Hessian
+        return self._coords.h - res.root * np.eye(h_n)
