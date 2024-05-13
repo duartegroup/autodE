16375f2a3abd0fded502efdc9935ff76461dddb3
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2024-05-13 16:05:26 +0100

    add test

--- tests/test_bracket/test_dhs.py
+++ tests/test_bracket/test_dhs.py
@@ -14,6 +14,7 @@ from autode.bracket.dhs import (
     TruncatedTaylor,
     DHSImagePair,
     ImageSide,
+    OptimiserStepError,
 )
 from autode.opt.coordinates import CartesianCoordinates
 from autode import Config
@@ -95,6 +96,32 @@ def test_distance_constrained_optimiser():
     prod_coords_new = opt.final_coordinates
     new_distance = np.linalg.norm(prod_coords_new - rct_coords)
     assert np.isclose(new_distance, distance)
+
+
+@work_in_zipped_dir(os.path.join(here, "data", "conopt.zip"))
+def test_dist_constr_optimiser_sd_fallback():
+    coords1 = CartesianCoordinates(np.loadtxt("last.txt"))
+    coords1.update_g_from_cart_g(np.loadtxt("last_g.txt"))
+    coords1.update_h_from_cart_h(np.loadtxt("last_h.txt"))
+    pivot = CartesianCoordinates(np.loadtxt("pivot.txt"))
+
+    # if lagrangian step fails, it should do a steepest descent
+    opt = DistanceConstrainedOptimiser(
+        pivot_point=pivot,
+        maxiter=2,
+        init_trust=0.2,
+        gtol=1e-3,
+        etol=1e-4,
+    )
+    opt._target_dist = 2.6869833732268
+    opt._history.open("test_trj")
+    opt._history.add(coords1)
+    with pytest.raises(OptimiserStepError):
+        opt._get_lagrangian_step(coords1, coords1.g)
+    # however, steepest descent step should work
+    sd_step = opt._get_sd_step(coords1, coords1.g)
+    opt._step()
+    assert np.allclose(opt._coords, coords1 + sd_step)
 
 
 def test_dhs_image_pair():
