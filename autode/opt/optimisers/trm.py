e5f44839ab2ff8aa70da792fdbc112eac47162de
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2024-07-08 11:32:49 +0100

    trm update

--- autode/opt/optimisers/trm.py
+++ autode/opt/optimisers/trm.py
@@ -86,29 +86,58 @@ class TRMOptimiser(CRFOptimiser):
             return np.linalg.norm(ds_atoms) - self.alpha
 
         min_b = self._coords.min_eigval
-        # try simple quasi-Newton first
+        # try simple quasi-Newton if hessian is positive definite
         if min_b > 0 and trm_step_error(0.0) < 0.0:
             step = get_trm_step(self._coords.h, self._coords.g, 0.0)
             self._take_step_within_trust_radius(step)
             return None
 
-        # root search
-        right_bound = min_b - 1.0e-6
-        assert trm_step_error(right_bound) > 0
-        left_bound = right_bound - 0.1
-        for _ in range(500):
-            if trm_step_error(left_bound) < 0:
-                break
-            left_bound -= 0.1
-        if trm_step_error(left_bound) > 0:
-            raise OptimiserStepError(
-                "Failed to obtain TRM step within trust radius"
-            )
+        # next try RFO step if within trust radius
+        rfo_shift = self._coords.rfo_shift
+        rfo_step = get_trm_step(self._coords.h, self._coords.g, rfo_shift)
+        if np.linalg.norm(rfo_step) <= self.alpha:
+            logger.info(f"Calculated RFO λ = {rfo_shift}")
+            self._take_step_within_trust_radius(rfo_step)
+            return None
 
-        res = root_scalar(f=trm_step_error, bracket=[left_bound, right_bound])
-        if not res.converged:
-            raise OptimiserStepError("Failed in root-finding")
-        logger.info(f"Calculated TRM λ = {res.root:.4f}")
-        step = get_trm_step(self._coords.h, self._coords.g, res.root)
-        self._take_step_within_trust_radius(step)
-        return None
+        # constrain step to trust radius
+        try:
+            d = 1.0
+            # bisection to find upper bound
+            for _ in range(500):
+                right_bound = min_b - d
+                if trm_step_error(right_bound) > 0:
+                    break
+                d = d / 2
+            assert trm_step_error(right_bound) > 0
+
+            left_bound = right_bound - 0.1
+            for _ in range(1000):
+                if trm_step_error(left_bound) < 0:
+                    break
+                left_bound -= 0.1
+            if trm_step_error(left_bound) > 0:
+                raise OptimiserStepError(
+                    "Unable to find bounds for root search"
+                )
+
+            res = root_scalar(
+                f=trm_step_error, bracket=[left_bound, right_bound]
+            )
+            if not res.converged:
+                raise OptimiserStepError("Root search did not converge")
+            logger.info(f"Calculated TRM λ = {res.root:.4f}")
+            step = get_trm_step(self._coords.h, self._coords.g, res.root)
+            self._take_step_within_trust_radius(step)
+            return None
+
+        # TRM failed, switch to scaled RFO
+        except OptimiserStepError as exc:
+            logger.info(
+                f"TRM step failed: {str(exc)}, switching to scaled RFO"
+            )
+            logger.info(f"Calculated RFO λ = {rfo_shift}")
+            # use scaled RFO
+            rfo_step = rfo_step * self.alpha / np.linalg.norm(rfo_step)
+            self._take_step_within_trust_radius(rfo_step)
+            return None
